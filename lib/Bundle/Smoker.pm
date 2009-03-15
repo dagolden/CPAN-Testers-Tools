09cbe8722d503d902640d6b951b79ea12fb488c3
David Golden <dagolden@cpan.org>
2009-03-15 15:39:03 -0400

    added bundle of common smoker tools

---
+++ lib/Bundle/Smoker.pm
@@ -0,0 +1,44 @@
+package Bundle::Smoker;
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=head1 CONTENTS
+
+Test::More
+
+File::HomeDir
+
+Parse::CPAN::Meta
+
+Bundle::CPAN
+
+Config::Tiny
+
+Devel::Autoflush
+
+File::pushd
+
+Fcntl
+
+File::Path
+
+Tee
+
+Probe::Perl
+
+Term::Title
+
+Test::Reporter
+
+CPAN::Reporter
+
+CPAN::Reporter::Smoker
+
+=cut
+
