826349665c4ea969ab5da74dd1505dd2cfd17bbc
David Golden <dagolden@cpan.org>
2009-04-27 10:42:52 -0400

    make more file paths absolute early

--- regression.pl
+++ regression.pl
@@ -11,13 +11,13 @@ my $suffix = qr{\.(?:tar\.(?:bz2|gz|Z)|t(?:gz|bz)|(?<!ppm\.)zip|pm.gz)$}i;
 my $dist_re = qr{[a-zA-Z]+/.+?$suffix};
 
 my @spec = (
-  Param(
-    "src|S", sub { -e "$_/makeaperl" or die "Not a perl source directory" }
-  )->required,
+  # required
+  Param( "src|S", sub { -e "$_/makeaperl" } )->required,
   Param("old|O", $dist_re)->required,
   Param("new|N", $dist_re)->required,
-  Param("dir|D")->required,
+  Param("dir|D", sub { -d } )->required,
   Param("list|L", sub { -r } )->required,
+  # optional
   Param("threads|t"),
   Param("temp|-T"),
   List("extra|x"), 
@@ -60,22 +60,28 @@ sub main {
   die "CPAN must be configured using ~/.cpan/CPAN/MyConfig.pm\n"
     unless -r file( $ENV{HOME}, qw/ .cpan CPAN MyConfig.pm / );
 
+  # make paths absolute before changing directories
+  my $perl_src = dir( $opt->get_src )->absolute;
+  my $output_dir = dir( $opt->get_dir )->absolute;
+  my $list = file($opt->get_list)->absolute;
+
   # setup temporary work directory
-  my $work_dir = $opt->get_temp ? pushd( $opt->get_temp ) : tempd();
-  my $perldir = dir($work_dir, 'perl'); 
-  my $perlbin = file($perldir, 'bin', 'perl');
-  my $old_report_dir = dir ( $opt->get_dir, 'reports-old' );
-  my $new_report_dir = dir ( $opt->get_dir, 'reports-new' );
+  my $work_dir = $opt->get_temp ? pushd( $opt->get_temp ) 
+                                : tempd();
+  my $perl_dir = dir($work_dir, 'perl'); 
+  my $perl_bin = file($perl_dir, 'bin', 'perl');
+  my $old_report_dir = dir ( $output_dir, 'reports-old' );
+  my $new_report_dir = dir ( $output_dir, 'reports-new' );
   
   print "*** Working directory is '$work_dir' ***\n";
 
   # build perl in work directory
-  build_perl( $opt, $perldir );
+  build_perl( $opt, $perl_src, $perl_dir );
 
   # install CPAN::Reporter::Smoker and extra modules
-  my @requires = qw(YAML CPAN::SQLite CPAN::Reporter::Smoker);
+  my @requires = qw(File::Temp YAML CPAN::SQLite CPAN::Reporter::Smoker);
   for my $mod ( @requires, $opt->get_extra ) {
-    cpan_install( $perlbin, $mod );
+    cpan_install( $perl_bin, $mod );
   }
 
   # archive perl directory
@@ -84,26 +90,26 @@ sub main {
     and die "Problem archiving perl dir. Stopping.\n"; 
 
   # smoke_it( old dist )
-  smoke_it( $opt, $work_dir, $old_report_dir, $perlbin, $opt->get_old );
+  smoke_it( $opt, $work_dir, $old_report_dir, $perl_bin, $opt->get_old, $list );
 
   # restore perl from archive file
   print "*** Restoring perl from archive ***\n";
-  eval { rmtree ($perldir); 1} 
+  eval { rmtree ($perl_dir); 1} 
     or die "Problem removing modified perl directory. Stopping\n";
   system( 'tar xf perl.tar') 
     and die "Problem extracting archived perl directory. Stopping\n";
 
   # smoke_it( new dist )
-  smoke_it( $opt, $work_dir, $new_report_dir, $perlbin, $opt->get_new );
+  smoke_it( $opt, $work_dir, $new_report_dir, $perl_bin, $opt->get_new, $list );
   
   # compare output directories
-  compare_results( $perlbin, $opt->get_dir, $old_report_dir, $new_report_dir); 
+  compare_results( $perl_bin, $output_dir, $old_report_dir, $new_report_dir); 
 
 }
 
 sub build_perl {
-  my ($opt, $target_dir) = @_;
-  my $pd = pushd( $opt->get_src );
+  my ($opt, $perl_src, $target_dir) = @_;
+  my $pd = pushd( $perl_src );
   print "*** Building perl from '$pd' to '$target_dir' ***\n";
   my $config_args="-des -Dprefix=$target_dir";
   $config_args .= " -Dusethreads" if $opt->get_threads;
@@ -117,22 +123,22 @@ sub build_perl {
 }
 
 sub cpan_install {
-  my ($perlbin, $mod) = @_;
+  my ($perl_bin, $mod) = @_;
   print "*** Installing $mod ***\n";
   local %ENV = (%ENV, _automated_testing_env());
-  system("$perlbin -MCPAN -e 'install(q{$mod})'")
+  system("$perl_bin -MCPAN -e 'install(q{$mod})'")
     and die "Problem installing CPAN::Reporter::Smoker. Stopping\n";
-  system("$perlbin -MCPAN -e 'exit !CPAN::Shell->expandany(q{$mod})->uptodate'")
+  system("$perl_bin -MCPAN -e 'exit !CPAN::Shell->expandany(q{$mod})->uptodate'")
     and die "Could not confirm $mod installed\n";
 }
 
 sub smoke_it {
-  my ($opt, $work_dir, $result_dir, $perlbin, $dist) = @_;
+  my ($opt, $work_dir, $result_dir, $perl_bin, $dist, $list) = @_;
   print "*** Preparing to smoke test with $dist ***\n";
   $result_dir = $result_dir->absolute;
 
   # install regression distfile
-  cpan_install( $perlbin, $dist );
+  cpan_install( $perl_bin, $dist );
 
   # create output directory for results
   mkpath( "$result_dir" );
@@ -150,16 +156,15 @@ transport = File $result_dir
 ENDCONFIG
 
   # smoke the list
-  my $list = $opt->get_list;
   print "*** Smoke testing distributions in '$list' ***\n";
   rmtree( File::Spec->catdir($ENV{HOME}, qw/.cpan build/) );
-  system( "$perlbin -MCPAN::Reporter::Smoker -e 'start(list => q{$list})'" );
+  system( "$perl_bin -MCPAN::Reporter::Smoker -e 'start(list => q{$list})'" );
   system('stty sane');
   
 }
 
 sub compare_results {
-  my ($perlbin, $result_dir, $dir1, $dir2) = @_; 
+  my ($perl_bin, $result_dir, $dir1, $dir2) = @_; 
 
   my @dir1 = `ls $dir1`;
   my @dir2 = `ls $dir2`;
@@ -167,7 +172,7 @@ sub compare_results {
   my %dir1;
   my %dir2;
 
-  my $checkarch = `$perlbin -V:archname`;
+  my $checkarch = `$perl_bin -V:archname`;
   my ($archname) = $checkarch =~ m/'([^']+)'/;
 
   for my $f ( @dir1 ) {
