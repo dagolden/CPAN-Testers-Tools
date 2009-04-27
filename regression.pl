057812df500b9e03e3d153730e9f822cb1f4d7b8
David Golden <dagolden@cpan.org>
2009-04-26 21:24:33 -0400

    regression test builds perl

--- regression.pl
+++ regression.pl
@@ -1,24 +1,25 @@
 #!/usr/bin/env perl
-use 5.010;
 use strict;
 use warnings;
-use App::CPAN::Mini::Visit;
-use Archive::Tar ();
 use Getopt::Lucid qw/:all/;
 use File::Path qw/mkpath rmtree/;
-use File::pushd qw/pushd/;
-use File::Temp ();
+use File::pushd qw/pushd tempd/;
+use Path::Class;
 
 my $suffix = qr{\.(?:tar\.(?:bz2|gz|Z)|t(?:gz|bz)|(?<!ppm\.)zip|pm.gz)$}i; 
 my $dist_re = qr{[a-zA-Z]+/.+?$suffix};
 
 my @spec = (
   Param(
-    "src|S", sub { -e "$_/makeaperl" or die "Not a perl source directory"
+    "src|S", sub { -e "$_/makeaperl" or die "Not a perl source directory" }
   )->required,
-  Param("old|O", $dist_re)->required,
-  Param("new|N", $dist_re)->required,
-  Param("dir|D")->required,
+#  Param("old|O", $dist_re)->required,
+#  Param("new|N", $dist_re)->required,
+#  Param("dir|D")->required,
+#  Param("list|L")->required,
+  Param("threads|t"),
+  Param("temp|-T"),
+  List("extra|x"), 
   Switch("help|h"),
 );
 
@@ -26,21 +27,25 @@ my $usage = << "ENDHELP";
 usage: $0 <required options> <other options>
 
 REQUIRED OPTIONS:
-  --src|-S      perl source directory
-  --old|-O      older distfile to test ("AUTHOR/TARBALL")
-  --new|-N      newer distfile to test ("AUTHOR/TARBALL")
-  --dir|-D      directory for results
-  --list|-L     file with list of dists to test
+  --src|-S    DIR       perl source directory
+  --old|-O    DISTFILE  older distfile to test ("AUTHOR/TARBALL")
+  --new|-N    DISTFILE  newer distfile to test ("AUTHOR/TARBALL")
+  --dir|-D    DIR       directory for results
+  --list|-L   FILE      file with list of dists to test
 
 OTHER OPTIONS:
-
+  --extra|-x  MOD       extra module to install (can be repeated)
+  --temp|-T   DIR       used in place of temporary directory         
+  --threads|-t          build perl with threads
+  --help|-h             usage guide
 ENDHELP
 
-my $opt = Getopt::Lucid::getopt( \@spec );
-
-if ( $opt->get_help ) {
-  say $usage and exit 
+# XXX nasty hack until Getopt::Lucid has better help
+if ( grep { /--help|-h/ } @ARGV ) {
+  print STDERR "$usage\n" and exit 
 }
+
+my $opt = Getopt::Lucid->getopt( \@spec );
 
 main( $opt );
 exit;
@@ -50,25 +55,59 @@ exit;
 sub main {
   my ($opt) = @_;
 
-  # build perl in temp directory
+  # setup temporary work directory
+  my $work_dir = $opt->get_temp ? pushd( $opt->get_temp ) | tempd();
+  my $perldir = dir($work_dir, 'perl'); 
+  my $perlbin = file($perldir, 'bin', 'perl');
+  
+  print "*** Working directory is '$work_dir' ***\n";
 
-  # install CPAN::Reporter::Smoker
+  # build perl in work directory
+  build_perl( $opt, $perldir );
+
+  # install CPAN::Reporter::Smoker and extra modules
+  for my $mod ( 'YAML', 'CPAN::Reporter::Smoker', $opt->get_extra ) {
+    print "*** Installing $mod  ***\n";
+    local %ENV = (%ENV, _automated_testing_env());
+    system("$perlbin -MCPAN -e 'install(q{$mod})'")
+      and die "Problem installing CPAN::Reporter::Smoker. Stopping\n";
+    system("$perlbin -M$mod -e 1")
+      and die "Could not confirm $mod installed\n";
+  }
 
   # archive perl directory
+  system("tar clpf perl.tar perl") 
+    and die "Problem archiving perl dir. Stopping.\n"; 
 
   # smoke_it( old dist )
+  smoke_it( $opt, $work_dir, $perlbin, $opt->get_old );
 
   # restore perl from archive file
+  eval { rmtree ($perldir); 1} 
+    or die "Problem removing modified perl directory. Stopping\n";
+  system( 'tar xf perl.tar') 
+    and die "Problem extracting archived perl directory. Stopping\n";
 
   # smoke_it( new dist )
-
+  smoke_it( $opt, $work_dir, $perlbin, $opt->get_new );
+  
   # compare output directories
 
 }
 
 sub build_perl {
   my ($opt, $target_dir) = @_;
-  
+  my $pd = pushd( $opt->get_src );
+  print "*** Building perl from '$pd' ***\n";
+  my $config_args="-des -Dprefix=$target_dir";
+  $config_args .= " -Dusethreads" if $opt->get_threads;
+  system("make realclean");
+  system("rm -f config.sh Policy.sh");
+  system("sh ./Configure $config_args") 
+    and die "Problem with Configuration. Stopping.\n";
+  system("make depend") and die "Problem with make depend. Stopping.\n";
+  system("make") and die "Problem with make. Stopping\n";
+  system("make install") and die "Problem with install. Stopping\n";
 }
 
 sub smoke_it {
@@ -84,5 +123,13 @@ sub smoke_it {
 
   # smoke the list
 
+}
+
+sub _automated_testing_env {
+  return (
+    AUTOMATED_TESTING => 1,
+    PERL_MM_USE_DEFAULT => 1,
+    PERL_EXTUTILS_AUTOINSTALL => '--default-deps',
+  );
 }
 
