595dce257b5a9820e60a84a2e7e173ad5fa768fb
David Golden <dagolden@cpan.org>
2009-07-01 10:26:54 -0400

    update regress helper for an M::B run

--- regress-it-restart.sh
+++ regress-it-restart.sh
@@ -2,6 +2,6 @@
 ~/git/cpan-testers-tools/regression.pl        \
   --tar                                       \
   --old EWILHELM/Module-Build-0.33.tar.gz     \
-  --new DAGOLDEN/Module-Build-0.33_02.tar.gz  \
+  --new DAGOLDEN/Module-Build-0.33_03.tar.gz  \
   --dir mb-comparison --list mb-list.txt      \
   --temp /home/david/tmp
