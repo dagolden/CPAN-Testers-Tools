0240099c66ea41a1a8caf3ac17e3938d6a6548da
David Golden <dagolden@cpan.org>
2009-11-14 21:35:02 -0500

    update start-regression for latest M::B test

--- start-regression.pl
+++ start-regression.pl
@@ -1,8 +1,8 @@
 #!/bin/bash
 ~/git/cpan-testers-tools/regression.pl        \
-  --src /home/david/src/perl-5.10.0/          \
-  --old EWILHELM/Module-Build-0.33.tar.gz     \
-  --new DAGOLDEN/Module-Build-0.33_02.tar.gz  \
+  --src /home/david/src/perl-5.10.1/          \
+  --old DAGOLDEN/Module-Build-0.35.tar.gz     \
+  --new DAGOLDEN/Module-Build-0.35_07.tar.gz  \
   --dir mb-comparison --list mb-list.txt      \
   --extra POE --extra Moose                   \
   --temp /home/david/tmp
