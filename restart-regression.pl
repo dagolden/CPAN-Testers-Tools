a4c999d933a7fc42873e1b016583a35963527e94
David Golden <dagolden@cpan.org>
2015-04-17 23:20:49 +0200

    fix up scripts

--- restart-regression.pl
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-~/git/cpan-testers-tools/regression.pl        \
-  --tar                                       \
-  --old EWILHELM/Module-Build-0.33.tar.gz     \
-  --new DAGOLDEN/Module-Build-0.33_03.tar.gz  \
-  --dir mb-comparison --list mb-list.txt      \
-  --temp /home/david/tmp
--- restart-regression.sh
+++ restart-regression.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-~/git/CPAN-Testers-Tools/regression.pl        \
-  --src /home/david/src/perl-5.10.1/          \
+~/git/cpan-testers-tools/regression.pl        \
+  --tar                                       \
   --old EXODIST/Test-Simple-1.001014.tar.gz   \
   --new EXODIST/Test-Simple-1.301001_104.tar.gz   \
   --dir comparison --list list.txt      \
